bc3613c392b9b2f0e753416d950389390af25f75
matias <matias@DESKTOP-VAFV3S6>
2019-05-23 10:10:51 -0300

    agrego los mensajes de proyecto para q implemente nelson

--- Cientopolis/src/proyecto/Proyecto.java
+++ Cientopolis/src/proyecto/Proyecto.java
@@ -1,5 +1,20 @@
 package proyecto;
+/*clase q implementa nelson*/
+
+import java.util.ArrayList;
 
 public class Proyecto {
-
+	String descripcion;
+	String proposito;
+	private ArrayList<Encuesta> encuestas=new ArrayList<Encuesta>();
+	public Proyecto(String _descripcion, String _proposito) {
+		descripcion=_descripcion;
+		proposito=_proposito;
+	}
+	
+	//mensajes a implementar,seguro algunos faltan
+	
+	public List<Encuesta> obtenerEncuestasFinalizadas();//solo las finalizadas
+	public List<Encuesta> obtenerEncuestas();//las encuestas del proyecto
+	public void agregarEncuesta(Encuesta _encuesta);//agrega una encuesta al proyecto
 }
