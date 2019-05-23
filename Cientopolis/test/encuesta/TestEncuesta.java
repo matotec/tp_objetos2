095c3a0e8e5064edabd3a2a417ba46ca8b5d3ba2
matias <matias@DESKTOP-VAFV3S6>
2019-05-23 09:26:12 -0300

    agrego test encuesta

---
+++ Cientopolis/test/encuesta/TestEncuesta.java
@@ -0,0 +1,14 @@
+package encuesta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEncuesta {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
