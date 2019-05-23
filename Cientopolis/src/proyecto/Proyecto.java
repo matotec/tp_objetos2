02ae237be053cd5538d148e9f61f806a00169c99
matias <matias@DESKTOP-VAFV3S6>
2019-05-23 08:39:32 -0300

    agrego clase y test proyecto

---
+++ Cientopolis/src/proyecto/Proyecto.java
@@ -0,0 +1,5 @@
+package proyecto;
+
+public class Proyecto {
+
+}
---
+++ Cientopolis/test/proyecto/testProyecto.java
@@ -0,0 +1,14 @@
+package proyecto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testProyecto {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
