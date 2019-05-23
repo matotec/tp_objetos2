5538d09d02b25a3afd6b6ac7229122e892261323
matias <matias@DESKTOP-VAFV3S6>
2019-05-23 09:01:52 -0300

    agrego test Encuestado

---
+++ Cientopolis/test/encuestado/TestEncuestado.java
@@ -0,0 +1,14 @@
+package encuestado;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEncuestado {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
