16ee60864ef5ac46dfc0a363d93f3224b72d37f3
matias <matias@DESKTOP-VAFV3S6>
2019-05-23 09:35:33 -0300

    agrego test respuesta

---
+++ Cientopolis/test/respuesta/TestRespuesta.java
@@ -0,0 +1,14 @@
+package respuesta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRespuesta {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
