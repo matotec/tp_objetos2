b883d3668c4d8cf4bb9f9dd7710b4213695fbf47
matias <matias@DESKTOP-VAFV3S6>
2019-05-26 13:24:41 -0300

    agrego casi todos los test falta implementarlos

--- Cientopolis/test/cientopolisApp/TestCientopolisApp.java
+++ Cientopolis/test/cientopolisApp/TestCientopolisApp.java
@@ -17,12 +17,41 @@ public class TestCientopolisApp {
 	}
 	
 	@Test
-	public void testSeEnviaMensajeDeCrearProyectoAInvestigador() {
+	public void testSeEnviaMensajeDeAgregarProyectoAInvestigador() {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testSeEnviaMensajeDeCrearEncuestaAInvestigador() {
+	public void testSeCreaProyecto() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSeEnviaMensajeDeAgregarEncuestaAInvestigador() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSeCreaEncuesta() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testObtenerRespuestasDeEncuesta() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testObtenerEncuestasDeProyecto() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testObtenerProyectosDeInvestigador() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testobtenerEncuestasFinalizadasConMayorCantidadDeRespuestas() {
 		fail("Not yet implemented");
 	}
 }
